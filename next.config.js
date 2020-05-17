7bde253e42256b4787437a056b80f2f0cec18799
Alejandro Vivas <ajvivas86@gmail.com>
2020-05-16 21:45:12 -0400

    Commented next config content

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+/* module.exports = {
   exportTrailinglash: true,
   exportPathMap: function () {
     const paths = {
@@ -9,4 +9,4 @@ module.exports = {
 
     return paths;
   },
-};
+}; */
